655563f0693ec9e177736558d43c2aa0397d7759
Pierre Philippon <pierre@agile-enabler.com>
2021-03-19 11:52:37 +0100

    create template

---
+++ index.js
@@ -0,0 +1,1 @@
+import Paragraphe from "./input/paragraphe/Paragraphe";
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./index.js",
+  output: {
+    path: path.resolve("components"),
+    filename: "Components.js",
+    libraryTarget: "commonjs2",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              ["@babel/preset-react"],
+              ["@babel/preset-env", { targets: "defaults" }],
+            ],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      react: path.resolve(__dirname, "./node_modules/react"),
+      "react-dom": path.resolve(__dirname, "./node_modules/react-dom"),
+    },
+  },
+  externals: {
+    // Don't bundle react or react-dom
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "React",
+      root: "React",
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "ReactDOM",
+      root: "ReactDOM",
+    },
+  },
+};
